ee025d33514208c446f5066f3ac930fb09116700
VenhMac <gpfm312@gmail.com>
2023-04-01 16:05:36 +0900

    [add] Board, News Entity

--- src/main/java/com/daily/issue/batch/domain/News.java
+++ src/main/java/com/daily/issue/batch/domain/News.java
@@ -1,4 +1,25 @@
 package com.daily.issue.batch.domain;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Setter
+@Getter
+@Entity(name = "news")
 public class News {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String provider;
+    private String title;
+    private String url;
+    private String thumbnail;
+    private LocalDateTime regDt;
 }
